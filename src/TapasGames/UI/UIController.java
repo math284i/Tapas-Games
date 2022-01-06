f4ca7819c013b17b252e0b2857aa38b7eb0ea07e
Anders Poulsen <s204423@student.dtu.dk>
2022-01-06 11:44:21 +0100

    Gave til Ulrik

--- src/TapasGames/UI/UIController.java
+++ src/TapasGames/UI/UIController.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -14,14 +17,23 @@ public class UIController extends Application {
     Rectangle2D screenSize;
     Group group = new Group();
     Label label = new Label("Big old titties");
+    HBox info;
+    GridPane buttons = new GridPane();
+    Button exit = new Button("Exit");
+    Button settings = new Button("Settings");
+    Button vts = new Button("Vote to skip");
 
     @Override
     public void start(Stage primaryStage){
         screenSize = Screen.getPrimary().getBounds();
+        buttons.add(exit,0,0,1,2);
+        buttons.add(settings,0,1,1,1);
+        buttons.add(vts,1,1,1,1);
         //Lav 3 "objekter"
         //Læg dem i en gruppe
         group.getChildren().add(label);
-        mainScene = new Scene(group,(int) (0.7*screenSize.getHeight()), (int) (0.7*screenSize.getWidth()));
+        mainScene = new Scene(group, (int) (0.7*screenSize.getWidth()),(int) (0.7*screenSize.getHeight()));
+        label.setText("" + mainScene.getWidth() + ", " + mainScene.getHeight());
         primaryStage.setScene(mainScene);
         primaryStage.show();
     }
