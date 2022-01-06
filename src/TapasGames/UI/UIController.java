1ade7e87e2b1808d69229bac94e7c605c660a79c
Anders Poulsen <s204423@student.dtu.dk>
2022-01-06 11:24:39 +0100

    Added UI-framework

--- src/TapasGames/UI/UIController.java
+++ src/TapasGames/UI/UIController.java
@@ -1,12 +1,32 @@
 package TapasGames.UI;
 
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-public class UIController {
+public class UIController extends Application {
 
-    public UIController() {
+    Scene mainScene;
+    Rectangle2D screenSize;
+    Group group = new Group();
+    Label label = new Label("Big old titties");
 
+    @Override
+    public void start(Stage primaryStage){
+        screenSize = Screen.getPrimary().getBounds();
+        //Lav 3 "objekter"
+        //Læg dem i en gruppe
+        group.getChildren().add(label);
+        mainScene = new Scene(group,(int) (0.7*screenSize.getHeight()), (int) (0.7*screenSize.getWidth()));
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
     }
 
+    public static void main(String[] args){
+        launch(args);
+    }
 }
