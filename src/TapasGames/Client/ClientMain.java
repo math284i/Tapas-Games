8b1c6c33bb3b3f0ea4fc9c7aca34ed94296469ff
Dyberg <mathias.rerup-dyberg@hotmail.com>
2022-01-06 11:10:47 +0100

    Initial files

---
+++ src/TapasGames/Client/ClientMain.java
@@ -0,0 +1,9 @@
+package TapasGames.Client;
+
+import TapasGames.UI.UIController;
+
+public class ClientMain {
+
+    //public ClientMain(UIController )
+
+}
---
+++ src/TapasGames/UI/UIController.java
@@ -0,0 +1,12 @@
+package TapasGames.UI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UIController {
+
+    public UIController() {
+
+    }
+
+}
