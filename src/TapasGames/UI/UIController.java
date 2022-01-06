8f1ea2a96f65cd61182f67471ba84eb4fc2af722
Anders Poulsen <s204423@student.dtu.dk>
2022-01-06 15:33:18 +0100

    UI framework part 1

--- src/TapasGames/UI/UIController.java
+++ src/TapasGames/UI/UIController.java
@@ -10,22 +10,29 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class UIController extends Application {
 
     Scene mainScene;
     Rectangle2D screenSize;
-    Group group = new Group();
     Label label = new Label("Big old titties");
-    HBox info;
     GridPane buttons = new GridPane();
+    GridPane grid = new GridPane();
     Button exit = new Button("Exit");
     Button settings = new Button("Settings");
     Button vts = new Button("Vote to skip");
     Separator separatorH = new Separator(Orientation.HORIZONTAL);
     Separator separatorV = new Separator(Orientation.VERTICAL);
+
+    //TEMP
+    Button controls = new Button("Controls");
+    Button score = new Button("Score");
+    Button game = new Button("Game");
+    Button chat = new Button("Chat");
 
     @Override
     public void start(Stage primaryStage){
@@ -34,22 +41,52 @@ public class UIController extends Application {
         exit.setMaxSize(Double.MAX_VALUE,Double.MAX_VALUE);
         settings.setMaxSize(Double.MAX_VALUE,Double.MAX_VALUE);
         vts.setMaxSize(Double.MAX_VALUE,Double.MAX_VALUE);
-        buttons.add(exit,0,0,2,1);
-        buttons.add(settings,0,1,1,1);
+        buttons.setPrefSize(Double.MAX_VALUE,Double.MAX_VALUE);
+        buttons.add(exit,0,0,1,2);
+        buttons.add(settings,1,0,1,1);
         buttons.add(vts,1,1,1,1);
+        buttons.setVgrow(exit, Priority.ALWAYS);
+        buttons.setVgrow(settings,Priority.ALWAYS);
+        buttons.setVgrow(vts,Priority.ALWAYS);
+        buttons.setHgrow(exit,Priority.ALWAYS);
+        buttons.setHgrow(settings,Priority.ALWAYS);
+        buttons.setHgrow(vts,Priority.ALWAYS);
         buttons.setVgap(5);
         buttons.setHgap(5);
 
+        //TEMP
+        game.setPrefSize(windowSize[0]*0.75,windowSize[1]*0.8);
+        buttons.setPrefSize(windowSize[0]*0.25,windowSize[1]*0.2);
+        controls.setPrefSize(windowSize[0]*0.25,windowSize[1]*0.2);
+        score.setPrefSize(windowSize[0]*0.25,windowSize[1]*0.2);
+        chat.setPrefSize(windowSize[0]*0.25,windowSize[1]);
 
-        info = new HBox(buttons,separatorV,label);
-        info.setPrefWidth(0.75*windowSize[0]);
-        info.setPrefHeight(0.2*windowSize[1]);
-        //Lav 3 "objekter"
-        //Læg dem i en gruppe
-        group.getChildren().addAll(info);
-        mainScene = new Scene(group, (int) windowSize[0],(int) windowSize[1]);
+        //grid.getStyleClass().add("-fx - background - fill: black, white; fx - background - insets: 0, 1;");
+        grid.add(game,0,0,15,16);
+        grid.add(buttons,0,16,5,4);
+        grid.add(controls,5,16,5,4);
+        grid.add(score,10,16,5,4);
+        grid.add(chat,15,0,5,20);
+
+        grid.setHgrow(game,Priority.ALWAYS);
+        grid.setVgrow(game,Priority.ALWAYS);
+        grid.setHgrow(buttons,Priority.ALWAYS);
+        grid.setVgrow(buttons,Priority.ALWAYS);
+        grid.setHgrow(controls,Priority.ALWAYS);
+        grid.setVgrow(controls,Priority.ALWAYS);
+        grid.setHgrow(score,Priority.ALWAYS);
+        grid.setVgrow(score,Priority.ALWAYS);
+        grid.setHgrow(chat,Priority.ALWAYS);
+        grid.setVgrow(chat,Priority.ALWAYS);
+
+        mainScene = new Scene(grid, (int) windowSize[0],(int) windowSize[1]);
         label.setText("" + mainScene.getWidth() + ", " + mainScene.getHeight());
         primaryStage.setScene(mainScene);
+        primaryStage.setResizable(false);
+        primaryStage.maximizedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue)
+                primaryStage.setMaximized(false);
+        });
         primaryStage.show();
     }
 
